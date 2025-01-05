4de3e1c495ab616cdd88936450bca9478a2c10c5
Gabriel Mendes <biell.mendes8@gmail.com>
2025-01-05 19:22:55 -0300

    chore: final projetc style

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -4,9 +4,17 @@ function Footer() {
     return (
         <footer>
             <div className="dsmeta-logo-container">
-                <p>
-                   <a href="https://www.linkedin.com/in/biellms/" target="_blank">Desenvolvido por  © Gabriel Mendes</a>
+                <p className="footer-text">
+                    <a href="https://www.linkedin.com/in/biellms/" target="_blank">Desenvolvido por  © Gabriel Mendes</a>
                 </p>
+                <div className='dsmeta-footer-link'>
+                    <a href="https://www.linkedin.com/in/gabriel-mendes-0706ab1b8" target="_blank">
+                        <img src="https://img.shields.io/badge/-Linkedin-blue" className="linkedin-icon" />
+                    </a>
+                    <a href="https://github.com/Biellms" target="_blank">
+                        <img src="https://img.shields.io/badge/-Github-gray" className="github-icon" />
+                    </a>
+                </div>
             </div>
         </footer>
     )
