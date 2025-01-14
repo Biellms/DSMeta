f92937a904a96c74ecd7c01b5401324a3d57affa
Gabriel Mendes <biell.mendes8@gmail.com>
2025-01-14 16:08:50 -0300

    feature: dvlp custom react-toast to request notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -2,17 +2,34 @@ import './styles.css'
 import icon from '../../../src/assets/img/send-not.svg'
 import axios from 'axios';
 import { BASE_URL } from '../../utils/request';
+import { Bounce, toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 type Props = {
   saleId: number;
 }
 
 function handleClickSendNotification(id: number) {
-  axios(`${BASE_URL}/sales/notifySaleUser/${id}`)
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    .then(response => {
-      console.log("SUCCESS!");
-    })
+  const notifyPromise = toast.promise(
+    axios(`${BASE_URL}/sales/notifySaleUser/${id}`),
+    {
+      pending: 'Sending notification...',
+      success: 'Notification sent successfully! ',
+      error: "Failed to send notification! I probably don't have money anymore to request the API! 😭",
+    },
+    {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      theme: "colored",
+      transition: Bounce,
+    }
+  );
+  
+  return notifyPromise;
 }
 
 function NotificationButton({ saleId }: Props) {
